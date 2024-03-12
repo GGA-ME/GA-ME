02c25226d76f2a080017b1884c9602e55c33b747
김윤민 <qkslzl101216@naver.com>
2024-03-12 14:24:52 +0900

    backend Initial Setting
    초기 세팅

---
+++ backend/src/main/java/ssafy/ggame/GgameApplication.java
@@ -0,0 +1,13 @@
+package ssafy.ggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgameApplication.class, args);
+	}
+
+}
