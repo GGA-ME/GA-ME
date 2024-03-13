d7d09c8e8d9867ae0fa6108f87d05d184d5aaf0f
unknown <chiru7080@daum.net>
2024-03-13 17:58:50 +0900

    Feat: 게임카드 모션추가 및 호버추가

--- frontend/src/components/commonUseComponents/GameCard.tsx
+++ frontend/src/components/commonUseComponents/GameCard.tsx
@@ -4,6 +4,15 @@ import style from './GameCard.module.css'
 
 const GameCard = ({ imageUrl, title, price }) => {
     return (
+        <motion.div 
+        initial={{ x: -60 }} // Start position off-screen to the left
+        animate={{ x: 0 }} // End at the natural position
+        transition={{ duration: 0.3 }} // Duration of the animation
+        whileHover={{
+          scale: [null, 1.4, 1.2],
+          transition: { duration: 0.3 },
+        }}
+      >
       <div className={`${style.card} w-48 rounded overflow-hidden text-white text-center`}>
         <img src={imageUrl} alt={title} className="w-full" />
         <div className="p-2">
@@ -11,6 +20,7 @@ const GameCard = ({ imageUrl, title, price }) => {
           <p className="text-sm">{price}</p>
         </div>
       </div>
+      </motion.div>
     );
   };
   
--- frontend/src/components/mainComponents/Game.tsx
+++ frontend/src/components/mainComponents/Game.tsx
@@ -1,15 +1,21 @@
 import GameCard from '../commonUseComponents/GameCard'
+import { motion } from 'framer-motion'
 
 function Game() {
+    const gameCards = Array.from({ length: 20 }, (_, index) => (
+        <GameCard
+          key={index}
+          imageUrl="/Gameicon.gif"
+          title="ELDEN RING"
+          price="₩ 64,800"
+        />
+      ));
 
-  return (
-        <div>
-          <GameCard
-            imageUrl="/Gameicon.gif"
-            title="ELDEN RING"
-            price="₩ 64,800"
-          />
-        </div>
-  );
-}
-export default Game;
+    return (
+        
+        <div className="grid grid-cols-4 gap-4">
+        {gameCards}
+      </div>
+      );
+    }
+    export default Game;
