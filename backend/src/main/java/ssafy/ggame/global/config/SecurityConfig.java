4c44d316f37ad98a14f414060f41af30e3567665
Rael <allice2283006@gmail.com>
2024-03-13 12:40:58 +0900

    Feat: "로그인 기능 구현"
    구글 OAuth 로그인
    
    Fixes: #3

---
+++ backend/src/main/java/ssafy/ggame/global/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package ssafy.ggame.global.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.web.SecurityFilterChain;
+import ssafy.ggame.domain.user.service.CustomOAuth2UserService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest().authenticated()
+                );
+                //oauth 관련 request 추가
+        return http.build();
+    }
+}
