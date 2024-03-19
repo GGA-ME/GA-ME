666c1a0c7ae814970341738b3e3f21053b793a06
Rael <allice2283006@gmail.com>
2024-03-19 10:25:55 +0900

    Fix: 접근 권한 수정
    모든 접근 허용
    
    Related to: #3

--- backend/src/main/java/ssafy/ggame/domain/user/security/OAuth2LoginSecurityConfig.java
+++ backend/src/main/java/ssafy/ggame/domain/user/security/OAuth2LoginSecurityConfig.java
@@ -19,11 +19,13 @@ public class OAuth2LoginSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                // CSRF 보호 비활성화
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
                         // /user/login 경로에 대한 요청은 누구나 접근할 수 있도록 설정합니다.
-                        .requestMatchers("/user/login").permitAll()
+                        //.requestMatchers("/user/login").permitAll()
                         // 나머지 모든 요청은 인증된 사용자만 접근할 수 있도록 설정합니다.
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .oauth2Login(oauth2 -> oauth2
                         .successHandler(successHandler) // 로그인 성공 핸들러 등록
