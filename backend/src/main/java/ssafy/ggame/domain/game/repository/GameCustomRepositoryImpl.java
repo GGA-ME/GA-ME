ac073b55de137190f80cd2b966d139b645775d5f
김윤민 <qkslzl101216@naver.com>
2024-03-20 18:00:31 +0900

    Refactor: 게임이름으로 찾기 수정중
    게임 이름으로 찾지만 prefer과 tags가 나오지 않는 실수
    queryDsl을 이용해서 해당 정보들을 담을 수 있도록 수정중

--- backend/src/main/java/ssafy/ggame/domain/game/repository/GameCustomRepositoryImpl.java
+++ backend/src/main/java/ssafy/ggame/domain/game/repository/GameCustomRepositoryImpl.java
@@ -1,15 +1,21 @@
 package ssafy.ggame.domain.game.repository;
 
+import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import ssafy.ggame.domain.game.dto.GameCardDto;
 import ssafy.ggame.domain.game.entity.Game;
+import ssafy.ggame.domain.search.dto.SearchLikeRequestDto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static ssafy.ggame.domain.game.entity.QGame.game;
+import static ssafy.ggame.domain.gameTag.entity.QGameTag.gameTag;
+import static ssafy.ggame.domain.prefer.entity.QPrefer.prefer;
+import static ssafy.ggame.domain.tag.entity.QTag.tag;
 
 
 @Repository
@@ -23,13 +29,44 @@ public class GameCustomRepositoryImpl implements GameCustomRepository{
         3. 결과 Dto로 다 변환해서 return
      */
     @Override
-    public List<GameCardDto> findByGameNameContaining(String keyword) {
-        List<Game> searchGames = queryFactory.select(
-                        game
+    public List<GameCardDto> findByGameNameContaining(SearchLikeRequestDto dto) {
+        //엔티티로 추출
+        List<GameCardDto> searchGames = queryFactory.select(
+                        Projections.constructor(
+                                GameCardDto.class,
+                                game.gameId.as("gameId"),
+                                game.gameName.as("gameName"),
+                                game.gameHeaderImg.as("gameHeaderImg"),
+                                game.gamePriceInitial.as("gamePriceInitial"),
+                                game.gamePriceFinal.as("gamePriceFinal"),
+                                game.gameDeveloper.as("gameDeveloper")
+                        )
                 ).from(game)
-                .where(game.gameName.containsIgnoreCase(keyword))
+                .where(game.gameName.containsIgnoreCase(dto.getKeyword()))
                 .orderBy(game.gameFinalScore.desc())
                 .fetch();
-        return searchGames.stream().map(Game::converToGameCardDto).toList();
+
+        //해당하는 게임들 id를 추출
+        List<Long> ids = searchGames.stream()
+                .map(GameCardDto::getGameId) // Game 객체에서 id를 추출
+                .toList();
+        //prefer 가져오기
+        List<Long> preferIds = queryFactory.select(
+                        prefer.preferId.game.gameId
+                ).from(prefer)
+                .where(prefer.preferId.game.gameId.in(ids).and(prefer.preferId.user.userId.eq(dto.getUserId())))
+                .fetch();
+        //prefer에 해당하는 애들만 true로 설정 하기
+        searchGames.forEach(game -> game.updateIsPrefer(preferIds.contains(game.getGameId())));
+        //해당하는 게임들 태그 가져오기
+        List<Tuple> fetch = queryFactory.select(
+                        game.gameId,
+                        tag.tagName
+                ).from(gameTag)
+                .join(gameTag.game, game)
+                .join(gameTag.tag, tag)
+                .where(gameTag.game.gameId.in(ids)).distinct().fetch();
+
+        return searchGames;
     }
 }
--- backend/src/main/java/ssafy/ggame/domain/search/Service/SearchService.java
+++ backend/src/main/java/ssafy/ggame/domain/search/Service/SearchService.java
@@ -1,9 +1,10 @@
 package ssafy.ggame.domain.search.Service;
 
 import ssafy.ggame.domain.game.dto.GameCardDto;
+import ssafy.ggame.domain.search.dto.SearchLikeRequestDto;
 
 import java.util.List;
 
 public interface SearchService {
-    List<GameCardDto> searchLikeGame (String keyword);
+    List<GameCardDto> searchLikeGame (SearchLikeRequestDto dto);
 }
