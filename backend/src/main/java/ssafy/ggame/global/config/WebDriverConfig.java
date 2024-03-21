0d4d1b5f2c767adafabd871a992ffd3bcf1675ec
JongKook <whdrnrdl789@naver.com>
2024-03-21 14:31:01 +0900

    Feat: ChromeDriverPath

--- backend/src/main/java/ssafy/ggame/global/config/WebDriverConfig.java
+++ backend/src/main/java/ssafy/ggame/global/config/WebDriverConfig.java
@@ -1,17 +1,24 @@
 package ssafy.ggame.global.config;
 
+import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebDriverConfig {
 
+    @Value("${selenium.driver}")
+    private String path;
     @Bean
     public WebDriver chromeDriver() {
-
+        System.out.println("드라이버 탐색");
+        System.out.println(path);
+//        System.setProperty("webdriver.chrome.driver", webDriverProperties.getDriver());
         // ChromeOptions 설정 (예: 헤드리스 모드)
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
