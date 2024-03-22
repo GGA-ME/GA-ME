6a75f1829a5881ee0226556ace4e147da2b92143
Rael <allice2283006@gmail.com>
2024-03-22 11:10:56 +0900

    Refactor: 스웨거 적용
    스웨거 적용
    
    Related to: #18

--- backend/src/main/java/ssafy/ggame/domain/userActionLog/controller/UserActionLogController.java
+++ backend/src/main/java/ssafy/ggame/domain/userActionLog/controller/UserActionLogController.java
@@ -1,18 +1,20 @@
 package ssafy.ggame.domain.userActionLog.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ssafy.ggame.domain.userActionLog.dto.UserActionLogResponseDto;
-import ssafy.ggame.domain.userActionLog.entity.UserActionLog;
+import ssafy.ggame.domain.userActionLog.dto.UserActionLogReqDto;
+import ssafy.ggame.domain.userActionLog.dto.UserActionLogResDto;
 import ssafy.ggame.domain.userActionLog.service.UserActionLogService;
 import ssafy.ggame.global.common.BaseResponse;
-import ssafy.ggame.global.common.StatusCode;
 
 import java.util.List;
 import java.util.Map;
 
+@Tag(name = "User Action Log Controller", description = "사용자 행동 패턴 API")
 @RestController
 @RequestMapping("/api/tracking")
 public class UserActionLogController {
@@ -24,16 +26,13 @@ public class UserActionLogController {
         this.userActionLogService = userActionLogService;
     }
 
+    // 스웨거에 schemes 항목에 표시하기 위해서는 컨트롤러의 리턴 타입에 명시를 해줘야 함
+    @Operation(description = "사용자 행동 패턴을 카산드라 DB에 저장")
     @PostMapping("/log")
-    public ResponseEntity<Object> logUserAction(@RequestBody Map<String, Object> requestData) {
-        UserActionLogResponseDto responseDto;
+    public ResponseEntity<BaseResponse<UserActionLogResDto>> logUserAction(@RequestBody UserActionLogReqDto requestData) {
+        UserActionLogResDto responseDto;
 
-        Integer userId = (Integer) requestData.get("userId");
-        String page = (String) requestData.get("page");
-        String action = (String) requestData.get("action");
-        List<Map<String, Object>> args = (List<Map<String, Object>>) requestData.get("args"); // "args"를 읽어옵니다.
-
-        responseDto = userActionLogService.loggingUserTagWeight(userId, page, action, args); // args를 넘깁니다.
+        responseDto = userActionLogService.loggingUserTagWeight(requestData.getUserId(), requestData.getPage(), requestData.getAction(), requestData.getArgs());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(new BaseResponse<>(responseDto));
     }
