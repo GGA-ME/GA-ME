d55a159d5a0003f7244e152349ef348e43c38a52
김하영 <ha09368@naver.com>
2024-03-22 12:21:17 +0900

    Refactor: Recommendation Controller에 swagger 적용
    recommendation 패키지 내 컨트롤러와 디티오에 swagger 적용

--- backend/src/main/java/ssafy/ggame/domain/recommendation/controller/RecommendationController.java
+++ backend/src/main/java/ssafy/ggame/domain/recommendation/controller/RecommendationController.java
@@ -1,5 +1,8 @@
 package ssafy.ggame.domain.recommendation.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,16 +20,23 @@ import java.util.Objects;
 @RestController
 @RequestMapping("/api/recommendations")
 @RequiredArgsConstructor
+@Tag(name = "Recommendation Controller", description = "Recommendation과 관련된 API")
 public class RecommendationController {
 
     private final RecommendationService recommendationService;
 
     @GetMapping("/popular")
+    @Operation(description = "인기 게임 추천")
     ResponseEntity<BaseResponse<List<GameCardDto>>> getPopularGameList(
+            @Parameter(name = "userId", description = "유저 아이디, 로그인하지 않으면 0으로 줘 야함", example = "1")
             @RequestParam(required=true, defaultValue = "0") Integer userId,
+            @Parameter(name = "codeId", description = "codeId(CAT/GEN), 전체라면 0으로 줘야함", example = "CAT")
             @RequestParam(required =true, defaultValue = "0") String codeId,
+            @Parameter(name = "tagId", description = "tagId, 전체라면 0으로 줘야함", example = "11")
             @RequestParam(required = true, defaultValue = "0") Short tagId,
+            @Parameter(name = "page", description = "페이지네이션, 이 값을 넘기지 않으면 자동으로 0이 들어감", example = "1")
             @RequestParam(name = "page", defaultValue = "0") int page,
+            @Parameter(name = "size", description = "size, 한번 요청시 가져올 데이터 개수, 안쓰면 100개씩 가져옴", example = "100")
             @RequestParam(name="size", defaultValue = "100") int size){
 
         List<GameCardDto> resultList = recommendationService.getPopularList(userId, codeId, tagId, page, size);
@@ -36,6 +46,7 @@ public class RecommendationController {
     }
 
     @GetMapping("/personal/{userId}")
+    @Operation(description = "사용자 가중치 기반 인기 게임 추천")
     ResponseEntity<BaseResponse<List<GameCardDto>>> getPersonalGameList(@PathVariable Integer userId){
        List<GameCardDto> resultList =  recommendationService.getPersonalList(userId);
 
@@ -44,6 +55,7 @@ public class RecommendationController {
 
 
     @PostMapping("/search")
+    @Operation(description = "게임 검색 추천")
     ResponseEntity<BaseResponse<List<GameCardDto>>> searchGameList(@RequestBody SearchGameRequestDto searchGameRequestDto){
         List<GameCardDto> resultList = recommendationService.searchGameList(searchGameRequestDto);
 
