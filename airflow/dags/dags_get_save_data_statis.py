37bf4ec45bfe2634c82ae4b9724db045ad0c41aa
이상훈 <dltkdgns0303@gmail.com>
2024-03-23 21:14:07 +0900

    Fix: 카산드라 적용 및 print 주석처리
    리뷰 저장 및 호출 로직 카산드라 DB 적용
    
    resolves : #8

--- airflow/dags/dags_get_save_data_statis.py
+++ airflow/dags/dags_get_save_data_statis.py
@@ -10,6 +10,7 @@ import nltk
 nltk.download('vader_lexicon')
 import math
 import logging
+from cassandra.cluster import Cluster
 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
@@ -36,11 +37,11 @@ def load_reviews_with_retry(game_id, max_reviews):
         
             return reviews_en
         except requests.exceptions.SSLError as e:
-            print(f"SSLError 발생: {e}")
-            print("재시도 중...")
+            #print(f"SSLError 발생: {e}")
+            #print("재시도 중...")
             retry_count += 1
             sleep(5)  # 재시도 전에 잠시 대기
-    print(f"최대 재시도 횟수({MAX_RETRIES})를 초과하여 리뷰를 가져오지 못했습니다.")
+    #print(f"최대 재시도 횟수({MAX_RETRIES})를 초과하여 리뷰를 가져오지 못했습니다.")
     return None
 
 
@@ -50,7 +51,7 @@ def get_game_ids(**kwargs):
     cursor = conn.cursor()
     cursor.execute("SELECT game_id FROM game")
     game_ids = [row[0] for row in cursor.fetchall()]
-    print(game_ids)
+    #print(game_ids)
     cursor.close()
     conn.close()
     # 현재 날짜와 시간을 가져옴
@@ -76,29 +77,48 @@ def process_reviews(num_batches, index, **kwargs):
     mysql_hook = MySqlHook(mysql_conn_id=MYSQL_CONN_ID)
     conn = mysql_hook.get_conn()
     cursor = conn.cursor()
+    # 카산드라 클러스터에 연결하기 위한 정보 설정
+    cassandra_cluster = Cluster(['j10e105.p.ssafy.io'])
+    cassandra_session = cassandra_cluster.connect('ggame')
     
     for game_id in game_id_batch:
 
+        # 1. 최신 리뷰 날짜 가져오기
+        # 카산드라에서 데이터를 가져오는 대신에 Python 코드에서 직접 최신 날짜를 계산
+        query_latest_review_date = f"SELECT review_updated_dt FROM review WHERE game_id = {game_id}"
+        rows = cassandra_session.execute(query_latest_review_date)
 
-        sql = f"SELECT MAX(review_updated_dt) FROM review WHERE game_id = {game_id}"
-        cursor.execute(sql)
-        latest_review_date = cursor.fetchone()[0]
+        # 최신 날짜를 초기화
+        latest_review_date = None
+
+        # 각 행을 반복하면서 최신 날짜 갱신
+        for row in rows:
+            review_date = row.review_updated_dt
+            if latest_review_date is None or review_date > latest_review_date:
+                latest_review_date = review_date
+                
+        if latest_review_date is not None:
+            latest_review_date = datetime.combine(latest_review_date, datetime.min.time())
+
+
 
         max_reviews = 10000
         reviews_en = load_reviews_with_retry(game_id, max_reviews)
 
         if len(reviews_en):
-            print("리뷰를 성공적으로 불러왔습니다.")
-            print('game_id =', game_id)
+            #print("리뷰를 성공적으로 불러왔습니다.")
+            #print('game_id =', game_id)
+            pass
         else:
-            print("리뷰를 불러오지 못했습니다.")
+            #print("리뷰를 불러오지 못했습니다.")
             continue
 
         if not reviews_en.data['reviews'] or len(reviews_en.data['reviews']) < 1:
-            print(f"No reviews found for game ID: {game_id}")
+            #print(f"No reviews found for game ID: {game_id}")
             continue
         else:
-            print('리뷰길이', len(reviews_en.data['reviews']))
+            #print('리뷰길이', len(reviews_en.data['reviews']))
+            pass
             
             
 
@@ -130,19 +150,19 @@ def process_reviews(num_batches, index, **kwargs):
         threshold = average_playtime * 0.1
         for review in sorted_reviews:
             cnt += 1
-            print('덱이 진행한 리뷰 개수', cnt)
+            #print('덱이 진행한 리뷰 개수', cnt)
             # 리뷰의 timestamp_created 값
             timestamp_created = review['timestamp_updated'] ## 컬럼 updated로 바꿔 줘야할 듯 ? db 날리고
             # UNIX timestamp를 datetime -> date 객체로 변환
             review_datetime = datetime.fromtimestamp(timestamp_created)
-            review_updated_dt = review_datetime.date()
+            review_updated_dt = review_datetime
             
             
             # game_id, review_id, review_content, review_is_good, review_updated_dt, 
             # review_playtime_at, review_playtime_total, review_playtime_recent
             # review_is_use, updated_dttm
             game_id = int(game_id)
-            print(type(review['recommendationid']), review['recommendationid'])
+            #print(type(review['recommendationid']), review['recommendationid'])
             review_id = int(review['recommendationid'])
             review_content = review['review']
             review_is_good = 'NULL'
@@ -159,56 +179,52 @@ def process_reviews(num_batches, index, **kwargs):
                 continue
 
             if latest_review_date is None or review_updated_dt > latest_review_date:
-                print('시간시간', review_updated_dt, latest_review_date)
+                #print('시간시간', review_updated_dt, latest_review_date)
                 # 리뷰 저장 코드 작성
                 scores = analyzer.polarity_scores(review_content)
                 # {'neg': 0.0, 'neu': 0.308, 'pos': 0.692, 'compound': 0.6249}
                 compound_score = scores['compound']
                 if compound_score >= 0:
-                    print("긍정적인 감정입니다.")
+                    #print("긍정적인 감정입니다.")
                     review_is_good = True
                     game_review_like_cnt += 1
                 else:
-                    print("부정적인 감정입니다.")
+                    #print("부정적인 감정입니다.")
                     review_is_good = False
                     game_review_unlike_cnt += 1
                 
                 # 1.3배 이상 늘어난 현재 플탐인 경우 True로 바꿔줌
-                if review_playtime_total >= 1.3 * review_playtime_at:
-                    print("신규 댓글 + 플탐 OOOOOOO")
-                    review_is_use = True
-                else:
-                    print("신규 댓글 + 플탐 XXXX")
-                    review_is_use = False
+                if review_playtime_total is not None and review_playtime_at is not None:
+                    if review_playtime_total >= 1.3 * review_playtime_at:
+                        review_is_use = True
                 
 
-                # print(game_id, review_id, review_content, review_is_good, review_updated_dt, review_playtime_at, review_playtime_total, review_playtime_recent, review_is_use, updated_dttm)
-                sql = """INSERT INTO review (game_id, review_id, review_content, review_is_good, review_updated_dt, review_playtime_at, review_playtime_total, review_playtime_recent, review_is_use, updated_dttm) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-                cursor.execute(sql, (game_id, review_id, review_content, review_is_good, review_updated_dt, review_playtime_at, review_playtime_total, review_playtime_recent, review_is_use, updated_dttm))
-                
-                conn.commit()
+                # 4. 리뷰 삽입
+                insert_query = """INSERT INTO review (game_id, review_id, review_content, review_is_good, review_updated_dt, review_playtime_at, review_playtime_total, review_playtime_recent, review_is_use, updated_dttm) 
+                                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+                cassandra_session.execute(insert_query, (game_id, review_id, review_content, review_is_good, review_updated_dt, review_playtime_at, review_playtime_total, review_playtime_recent, review_is_use, updated_dttm))
+
 
             else:
                 # 1.3배 이상 늘어난 현재 플탐인 경우 True로 바꿔줌
-                if review_playtime_total >= 1.3 * review_playtime_at:
-                    print("존재하는 댓글 + 플탐 해당 OOOOOOOO")
-                    review_is_use = True
-                    
+                if review_playtime_total is not None and review_playtime_at is not None:
+                    if review_playtime_total >= 1.3 * review_playtime_at:
+                        #print("존재하는 댓글 + 플탐 해당 OOOOOOOO")
+                        review_is_use = True
+                        
+                        # 3. 리뷰 업데이트
+                        update_query = """UPDATE review 
+                                        SET review_is_use = %s, updated_dttm = %s
+                                        WHERE game_id = %s AND review_id = %s"""
+                        cassandra_session.execute(update_query, (review_is_use, updated_dttm, game_id, review_id))
 
 
-                    sql = """UPDATE review 
-                            SET review_is_use = %s, updated_dttm = %s
-                            WHERE game_id = %s AND review_id = %s"""
-                    cursor.execute(sql, (review_is_use, updated_dttm, game_id, review_id))
-                    
-                    conn.commit()
-
-                else:
-                    print("존재하는 댓글 + 플탐 해당 X")
-                    review_is_use = False
+                    else:
+                        #print("존재하는 댓글 + 플탐 해당 X")
+                        review_is_use = False
 
         try:
-            # print(game_id, review_content, review_is_good, review_updated_dt, review_playtime_at, review_playtime_total, review_playtime_recent, review_is_use, updated_dttm)
+            # #print(game_id, review_content, review_is_good, review_updated_dt, review_playtime_at, review_playtime_total, review_playtime_recent, review_is_use, updated_dttm)
             sql = """
                 INSERT INTO game_score_info (game_id, game_review_cnt, game_review_like_cnt, game_review_unlike_cnt, updated_dt) 
                 VALUES (%s, %s, %s, %s, %s) 
@@ -220,12 +236,14 @@ def process_reviews(num_batches, index, **kwargs):
             """
             cursor.execute(sql, (game_id, game_review_cnt, game_review_like_cnt, game_review_unlike_cnt, updated_dt))
             conn.commit()
-            print("Review 데이터가 성공적으로 저장되었습니다.")
+            #print("Review 데이터가 성공적으로 저장되었습니다.")
         except Exception as e:
             conn.rollback()
-            print("Review 데이터 저장 중 오류 발생:", e)
+            #print("Review 데이터 저장 중 오류 발생:", e)
     cursor.close()
     conn.close()
+    cassandra_session.shutdown()
+    cassandra_cluster.shutdown()
 
      
      
@@ -241,33 +259,40 @@ def process_statistics(num_batches, index, **kwargs):
     mysql_hook = MySqlHook(mysql_conn_id=MYSQL_CONN_ID)
     conn = mysql_hook.get_conn()
     cursor = conn.cursor()
+    
+    # 카산드라 클러스터에 연결하기 위한 정보 설정
+    cassandra_cluster = Cluster(['j10e105.p.ssafy.io'])
+    cassandra_session = cassandra_cluster.connect('ggame')
+    
 
     for game_id in game_id_batch:
         
-        # game_id에 해당하는 리뷰의 정보를 가져오는 쿼리
-        query = f"SELECT review_playtime_at, review_playtime_recent, review_is_use, review_is_good FROM review WHERE game_id = {game_id}"
-        cursor.execute(query)
-        reviews = cursor.fetchall()
+        # 2. 게임 아이디에 해당하는 리뷰 정보 가져오기
+        query_reviews = f"SELECT review_playtime_at, review_playtime_recent, review_is_use, review_is_good FROM review WHERE game_id = {game_id}"
+        reviews = cassandra_session.execute(query_reviews)
+
         
         # result가 비어 있는 경우 처리
         if reviews is not None:
-            print("리뷰를 성공적으로 불러왔습니다.")
-            print('game_id =', game_id)
+            #print("리뷰를 성공적으로 불러왔습니다.")
+            #print('game_id =', game_id)
+            pass
         else:
-            print("리뷰를 불러오지 못했습니다.")
-            print('game_id =', game_id)
+            #print("리뷰를 불러오지 못했습니다.")
+            #print('game_id =', game_id)
             continue
         
         if not reviews or len(reviews) < 1:
-            print(f"No reviews found for game ID: {game_id}")
+            #print(f"No reviews found for game ID: {game_id}")
             continue
         else:
-            print('리뷰 데이터', reviews)
-            print('리뷰길이', len(reviews))
+            #print('리뷰 데이터', reviews)
+            #print('리뷰길이', len(reviews))
+            pass
             
         # 불러온 리뷰 리스트에서 game_review_is_use_cnt의 합계를 계산
         total_game_review_is_use_cnt = sum(review[2] for review in reviews)
-        print("game_review_is_use_cnt의 총합:", total_game_review_is_use_cnt)
+        #print("game_review_is_use_cnt의 총합:", total_game_review_is_use_cnt)
         
         query = f"UPDATE game_score_info SET game_review_is_use_cnt = {total_game_review_is_use_cnt} WHERE game_id = {game_id}"
         cursor.execute(query)
@@ -278,8 +303,8 @@ def process_statistics(num_batches, index, **kwargs):
         # 값이 0 이상인 리뷰의 개수 계산
         positive_reviews_count = sum(1 for review in reviews if review[1] >= 0)
 
-        print("review_playtime_recent의 총합:", total_playtime_recent)
-        print("값이 0 이상인 리뷰의 개수:", positive_reviews_count)
+        #print("review_playtime_recent의 총합:", total_playtime_recent)
+        #print("값이 0 이상인 리뷰의 개수:", positive_reviews_count)
         
         
         recent_score = total_playtime_recent + positive_reviews_count*100
@@ -298,17 +323,17 @@ def process_statistics(num_batches, index, **kwargs):
         
 
         # 리뷰의 평균 플레이타임 계산
-        print(reviews)
+        #print(reviews)
         total_playtime = sum(review[0] for review in reviews)
         game_standard_playtime = total_playtime // len(reviews)
         if game_standard_playtime < 1:
             continue
-        print('game_standard 계산 전', total_playtime, len(reviews))
+        #print('game_standard 계산 전', total_playtime, len(reviews))
 
         # 시간대별로 긍정/부정 개수 계산
         positive_counts, negative_counts = count_reviews_by_time_bins(reviews, game_standard_playtime)
-        print("시간대별 긍정 리뷰 개수:", positive_counts)
-        print("시간대별 부정 리뷰 개수:", negative_counts)
+        #print("시간대별 긍정 리뷰 개수:", positive_counts)
+        #print("시간대별 부정 리뷰 개수:", negative_counts)
 
         # SQL 쿼리 작성
         sql = """
@@ -335,7 +360,8 @@ def process_statistics(num_batches, index, **kwargs):
     # 연결 종료
     cursor.close()
     conn.close()
-    
+    cassandra_session.shutdown()
+    cassandra_cluster.shutdown()
 
 # 리뷰 데이터에서 시간대별 긍정/부정 개수를 계산하는 함수
 def count_reviews_by_time_bins(reviews, average_playtime):
@@ -373,10 +399,11 @@ def get_game_final_score(index, num_batches, **kwargs):
         game_ids = kwargs["ti"].xcom_pull(task_ids = 'game_ids_task', key="game_ids")
 
         if not game_ids:
-            print('applist :', game_ids)
+            #print('applist :', game_ids)
             return
         else:
-            print('applist 존재')
+            #print('applist 존재')
+            pass
 
         game_id_batch = game_ids[index::num_batches]
 
@@ -388,7 +415,7 @@ def get_game_final_score(index, num_batches, **kwargs):
 
             for game_id in game_id_batch:
 
-                print("################### CALCULATE GAME SCORE!!!#######################")
+                #print("################### CALCULATE GAME SCORE!!!#######################")
 
                 # 가장 최근 갱신된 점수 정보 가져오기
                 select_query = f'''select 
@@ -405,7 +432,7 @@ def get_game_final_score(index, num_batches, **kwargs):
                 cursor.execute(select_query) 
 
                 select_result = cursor.fetchall()
-                print("SELECT_RESULT:: " ,select_result)
+                #print("SELECT_RESULT:: " ,select_result)
 
                 # 필요한 값 추출
                 game_id = select_result[0][0]
@@ -426,7 +453,7 @@ def get_game_final_score(index, num_batches, **kwargs):
                 update_query = f"""update game set game_final_score ={game_final_score} where game_id ={game_id}"""
                 cursor.execute(update_query)
                 conn.commit()
-                print("game_final_score 업데이트 완료!")
+                #print("game_final_score 업데이트 완료!")
 
         except Exception as e:
             log.fetal("get_game_score에서 예외 발생:: ", e)
@@ -437,7 +464,7 @@ with DAG('dags_get_save_data_statis',
          schedule_interval='@daily', 
          catchup=False) as dag:
     
-    num_batches = 100  # 등분할 개수
+    num_batches = 10  # 등분할 개수
     game_ids_task = PythonOperator(
         task_id='game_ids_task',
         python_callable=get_game_ids,
