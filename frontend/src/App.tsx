c6618e2a84a18a7fb56a0bb47b21971f938f9b49
JongKook <whdrnrdl789@naver.com>
2024-03-25 01:47:56 +0900

    Feat: survey page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import Search from './pages/Search';
 import Detail from './pages/Detail';
 import MyPage from './pages/MyPage';
 import Topic from './pages/HotTopic';
-import Suvey from './pages/Suvey';
+import Survey from './pages/Survey';
 // Google OAuth callback 처리 컴포넌트
 import CallbackComponent from './components/commonUseComponents/CallbackComponent'; 
 
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
   // 토픽 페이지
   { path: 'topic', element: <Topic/>},
   // 선호도 조사 페이지
-  { path: 'suvey', element: <Suvey/> }, // 날짜,차량,시간,위치,짐종류 같은거 제출
+  { path: 'survey', element: <Survey/> }, // 날짜,차량,시간,위치,짐종류 같은거 제출
   // Google OAuth 리다이렉트 처리
   { path: 'auth/google/callback', element: <CallbackComponent /> },
 ]);
