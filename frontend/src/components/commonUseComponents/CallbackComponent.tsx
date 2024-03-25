020f026413f7fde8e8e1e77b1a32d1cae3c827b3
Rael <allice2283006@gmail.com>
2024-03-25 13:55:12 +0900

    Fix: 구글 로그인 백엔드 전송 링크 수정

--- frontend/src/components/commonUseComponents/CallbackComponent.tsx
+++ frontend/src/components/commonUseComponents/CallbackComponent.tsx
@@ -12,16 +12,18 @@ const CallbackComponent: React.FC = () => {
   useEffect(() => {
     const fetchUser = async (code: string) => {
       try {
+        console.log('before call fetchUserInfo');
         // 서버로부터 사용자 정보를 받아옴
         const data = await fetchUserInfo(code);
+        console.log('after call fetchUserInfo');
         // 받아온 사용자 정보를 store에 저장
         setUser(data.user);
-        // 사용자를 /home으로 리다이렉트
-        navigate('/home');
+        // 사용자를 home으로 리다이렉트
+        navigate('/');
       } catch (error) {
         console.error('Authentication failed:', error);
         // 실패 시 로그인 페이지로 리다이렉트
-        navigate('/login');
+        navigate('/');
       }
     };
 
