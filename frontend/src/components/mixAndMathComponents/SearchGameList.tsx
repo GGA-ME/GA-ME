d9693a4d47b65229497828271f7a13fd5fb54322
김하영 <ha09368@naver.com>
2024-03-26 15:11:22 +0900

    Feat: 믹스앤매치 구현 순서 정하기
    믹스앤 매치 구현순서를 주석으로 달아놓음

--- frontend/src/components/mixAndMathComponents/SearchGameList.tsx
+++ frontend/src/components/mixAndMathComponents/SearchGameList.tsx
@@ -1,89 +1,21 @@
 import React from "react";
 import GameCard from "../commonUseComponents/GameCard";
 
-// 사용 스토어의 구조를 기반으로 하는 구조
-interface Game {
-  gameId: number;
-  gameName: string;
-  gameHeaderImg: string;
-  gamePriceFinal: number;
-  // 각 태그를 기준으로 각 태그 및 이름을 가진 경우 선언방법
-  tagList: Array<{ codeId: string; tagName: string }>;
-}
-
-const games: Game[] = [
-  {
-    gameId: 1,
-    gameName: "The Witcher 3: Wild Hunt",
-    gameHeaderImg: "witcher3.jpg",
-    gamePriceFinal: 29.99,
-    tagList: [
-      { codeId: "RPG", tagName: "Role-playing Game" },
-      { codeId: "OpenWorld", tagName: "Open World" },
-      { codeId: "Fantasy", tagName: "Fantasy" },
-    ],
-  },
-  {
-    gameId: 2,
-    gameName: "Red Dead Redemption 2",
-    gameHeaderImg: "rdr2.jpg",
-    gamePriceFinal: 39.99,
-    tagList: [
-      { codeId: "Action", tagName: "Action" },
-      { codeId: "Adventure", tagName: "Adventure" },
-      { codeId: "OpenWorld", tagName: "Open World" },
-    ],
-  },
-  {
-    gameId: 3,
-    gameName: "The Legend of Zelda: Breath of the Wild",
-    gameHeaderImg: "zelda.jpg",
-    gamePriceFinal: 49.99,
-    tagList: [
-      { codeId: "Action", tagName: "Action" },
-      { codeId: "Adventure", tagName: "Adventure" },
-      { codeId: "OpenWorld", tagName: "Open World" },
-      { codeId: "Fantasy", tagName: "Fantasy" },
-    ],
-  },
-];
-
-// dummy data for test end
-
-interface SearchGameListProps {
-  handleCombination: () => Promise<any>; // handleCombination 함수의 타입 정의
-}
+// TODO: 1. pocket에 담긴 게임 가져오기
+// TODO: 2. 가져온 포켓게임 보여주기
+// TODO: 3. MixAndMatchStore에 fetchData axios 만들기
+// TODO: 4. 조합하기 버튼 만들기*(axios) 연결
+// TODO: 5. 조합하기 버튼 onClick handle함수 만들기
 
 
-const SearchGameList: React.FC<SearchGameListProps> = ({ handleCombination }) => {
 
-  const handleClick = async () => {
-    try {
-      console.log("handleClick run! in SearchGameList")
-      const result = await handleCombination(); // handleCombination 함수 호출
-      console.log("Combination result:", result);
-    } catch (error) {
-      console.error("Error occurred while handling combination:", error);
-    }
-  };
 
 
 
   return (
     <>
     <div>
-      {games.map((game: Game, index: number) => {
-        return (
-          <GameCard
-            key={index}
-            imageUrl={`./TestGameImg.jpg`}
-            title={game.gameName}
-            price={`₩ ${game.gamePriceFinal}`}
-            tags={game.tagList.map((tag) => tag.tagName)}
-            likes={34}
-          />
-        );
-      })}
+      
       </div>
       <button onClick={handleClick}>조합하기</button>
     </>
