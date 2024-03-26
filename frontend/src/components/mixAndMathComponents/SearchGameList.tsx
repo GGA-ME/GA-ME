264d28eb5225bb5a510adb3adb8a0a955cb73e3a
김하영 <ha09368@naver.com>
2024-03-26 15:43:34 +0900

    Feat: 믹스앤매치 css 파일 추가
    믹스앤매치 css 파일 추가
    
    Related to: #24

--- frontend/src/components/mixAndMathComponents/SearchGameList.tsx
+++ frontend/src/components/mixAndMathComponents/SearchGameList.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import usePoketStore from "../../stores/poketStore";
 import GameCard from "../commonUseComponents/GameCard";
+import style from "./MixandMatch.module.css"
 
 // TODO: 1. pocket에 담긴 게임 가져오기 DONE
 // TODO: 2. 가져온 포켓게임 보여주기 DONE
-// TODO: 3. MixAndMatchStore에 fetchData axios 만들기
+// TODO: 3. MixAndMatchStore에 fetchData axios 만들기 DONE
 // TODO: 4. 조합하기 버튼 만들기*(axios) 연결
 // TODO: 5. 조합하기 버튼 onClick handle함수 만들기
 
@@ -16,6 +17,7 @@ const SearchGameList: React.FC = () => {
 
 
   return (
+    <div className={style.box}>
    <div>
     {cartItems.map((item)=>(
       <GameCard
@@ -29,6 +31,8 @@ const SearchGameList: React.FC = () => {
 
       />
     ))}
+   </div>
+   <button> 조합하기 </button>
    </div>
   );
 };
