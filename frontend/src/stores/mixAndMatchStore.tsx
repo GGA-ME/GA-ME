0fb9976c3a1280b93f56daddd15ad7f4ceedb56d
김하영 <ha09368@naver.com>
2024-03-26 15:40:29 +0900

    Feat: 믹스앤매치 - axios요청 함수 추가
    MixAndMatchStore에 fetchData axios 만들기
    
    Related to: #24

--- frontend/src/stores/mixAndMatchStore.tsx
+++ frontend/src/stores/mixAndMatchStore.tsx
@@ -1,10 +1,33 @@
-import { create } from 'zustand'
-import axios, { AxiosError } from 'axios';
+import { create } from "zustand";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://j10e105.p.ssafy.io",
+  headers: {
+    "Content-Type": `application/json;charset=UTF-8`,
+    Accept: "application/json",
+    // 추가
+    "Access-Control-Allow-Origin": `http://localhost:5173/`,
+    "Access-Control-Allow-Credentials": "true",
+  },
+});
+
+interface RequestData {
+  userId: number;
+  gameIdAndTagDtoList: {
+    gameId: number;
+    tagList: {
+      codeId: string;
+      tagId: number;
+      tagName: string;
+    }[];
+  }[];
+}
 
 interface Tag {
-    codeId: string;
-    tagId: number;
-    tagName: string;
+  codeId: string;
+  tagId: number;
+  tagName: string;
 }
 
 interface SearchResult {
@@ -19,75 +42,34 @@ interface SearchResult {
 }
 
 interface SearchState {
-    loading: boolean,
-    error: AxiosError | null,
-    results: SearchResult[]; // 검색 결과를 저장할 상태
-    setResults: (results: SearchResult[]) => void; // 검색 결과를 업데이트하는 액션
-  }
-
-// API 응답 데이터의 타입을 정의합니다.
-interface ApiResponse {
-    isSuccess: boolean;
-    message: string;
-    code: number;
-    result: {
-        tagDtoList: Tag[];
-        gameCardDtoList: SearchResult[];
-    }; // `any` 대신 더 구체적인 타입을 사용해주세요.
+  results: SearchResult[]; // 검색 결과를 저장할 상태
+  setResults: (results: SearchResult[]) => void; // 검색 결과를 업데이트하는 액션
 }
 
-const api = axios.create({
-    baseURL: 'https://j10e105.p.ssafy.io',
-    headers: {
-        "Content-Type": `application/json;charset=UTF-8`,
-        "Accept": "application/json",      
-        // 추가  
-        "Access-Control-Allow-Origin": `http://localhost:5173/`,
-        'Access-Control-Allow-Credentials':"true",
+const mixAndMatchStore = create<SearchState>((set) => ({
+
+  results: [], // 초기 상태는 빈 배열
+  setResults: (results) => set({ results }), // 검색 결과 업데이트
+
+  fetchData: async (postData: RequestData) => {
+    try {
+      console.log("store!!!");
+      const response = await api.post(
+        `/api/recommendations/search`,
+        postData
+      );
+
+      // Zustand 스토어에 응답 데이터를 저장합니다.
+      set({ results: response.data });
+      console.log("mixAndMatchStore response: ", response.data);
+      return response.data;
+
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.log(error)
+      }
     }
-  });
-
-
-interface RequestData {
-    userId: number;
-    gameIdAndTagDtoList: {
-      gameId: number;
-      tagList: {
-        codeId: string;
-        tagId: number;
-        tagName: string;
-      }[];
-    }[];
-  }
-
-
-
-
-const mixAndMatchStore = create<SearchState>((set) =>({
-    // data: null,
-    loading: false,
-    error: null,
-
-    results: [],// 초기 상태는 빈 배열
-    setResults: (results) => set({ results }), // 검색 결과 업데이트
-
-    fetchData: async (postData: RequestData) => {
-        try {
-            console.log("store!!!")
-            const response = await api.post<ApiResponse>(`/api/recommendations/search`, postData);
-            set({ loading: true });
-            // Zustand 스토어에 응답 데이터를 저장합니다.
-            //set({ result: response.data.result, loading: false });
-            console.log("mixAndMatchStore response: ", response.data);
-            return response.data;
-            // return response
-        } catch (error) {
-            if (axios.isAxiosError(error)) {
-                set({ error, loading: false });
-            }
-        }
-    }
-
+  },
 }));
 
 export default mixAndMatchStore;
