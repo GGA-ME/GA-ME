276faa63868be80e687f8a004524cc5f0fb8413a
김하영 <ha09368@naver.com>
2024-03-26 15:34:35 +0900

    Feat: 믹스앤매치 - 카트에 담긴 게임 정보 보여주기
    믹스앤매치 - 카트에 담긴 게임 정보 보여주기
    
    Related to: #24

--- frontend/src/components/mixAndMathComponents/SearchGameList.tsx
+++ frontend/src/components/mixAndMathComponents/SearchGameList.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import usePoketStore from "../../stores/poketStore";
+import GameCard from "../commonUseComponents/GameCard";
 
 // TODO: 1. pocket에 담긴 게임 가져오기 DONE
-// TODO: 2. 가져온 포켓게임 보여주기
+// TODO: 2. 가져온 포켓게임 보여주기 DONE
 // TODO: 3. MixAndMatchStore에 fetchData axios 만들기
 // TODO: 4. 조합하기 버튼 만들기*(axios) 연결
 // TODO: 5. 조합하기 버튼 onClick handle함수 만들기
+
 
 const SearchGameList: React.FC = () => {
 
@@ -14,7 +16,20 @@ const SearchGameList: React.FC = () => {
 
 
   return (
-   <div></div>
+   <div>
+    {cartItems.map((item)=>(
+      <GameCard
+        gameId={item.gameId}
+        imageUrl={item.imageUrl}
+        title={item.title}
+        price={item.price}
+        tags={item.tags}
+        likes={0} // 임시 값
+        onGameClick={()=>console.log("temp onGameClick: ", item.title )}
+
+      />
+    ))}
+   </div>
   );
 };
 
