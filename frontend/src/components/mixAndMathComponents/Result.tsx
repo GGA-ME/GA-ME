f466d82cdf96f227326101102cad78649f6a43d5
김하영 <ha09368@naver.com>
2024-03-26 17:52:34 +0900

    Feat: 믹스앤매치 tagDto 반영
    믹스앤매치 tagDto 반영
    
    Related to: #24

---
+++ frontend/src/components/mixAndMathComponents/Result.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Result: React.FC = () => {
+;
+
+    return(
+        <>
+        <h1>검색 추천 결과</h1>
+        <div>
+            
+        </div>
+        </>
+    )
+}
+
+export default Result
