d29de3cf4418bef06f441f63cf55fef67b733311
김하영 <ha09368@naver.com>
2024-03-28 10:37:34 +0900

    Fix: 검색 추천 검색 게임 태그 빈도수 수정
    검색 게임 추천에서 검색게임의 태그 빈도수 세는 로직 수정
    디티오 내용이 같아도 다른 객체로 인식하는 문제 해결

--- backend/src/main/java/ssafy/ggame/domain/tag/dto/TagDto.java
+++ backend/src/main/java/ssafy/ggame/domain/tag/dto/TagDto.java
@@ -2,6 +2,8 @@ package ssafy.ggame.domain.tag.dto;
 
 import lombok.*;
 
+import java.util.Objects;
+
 @Builder
 @Getter
 @NoArgsConstructor
@@ -11,4 +13,23 @@ public class TagDto {
     String codeId;
     Short tagId;
     String tagName;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        TagDto other = (TagDto) obj;
+        return Objects.equals(codeId, other.codeId)
+                && Objects.equals(tagId, other.tagId)
+                && Objects.equals(tagName, other.tagName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codeId, tagId, tagName);
+    }
 }
