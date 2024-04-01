9f585b0f8da9449ced199dfb3d5c4649ba099e1c
unknown <chiru7080@daum.net>
2024-04-01 19:47:48 +0900

    Fix: like함수 재수정

--- frontend/src/components/commonUseComponents/GameCard.tsx
+++ frontend/src/components/commonUseComponents/GameCard.tsx
@@ -76,11 +76,11 @@ const GameCard: React.FC<GameCardProps> = ({ gameId, imageUrl, title, developer,
       if (isLiked) {
         await unlikeGame(); // 좋아요 취소 요청
         setIsLiked(!isPrefer)
-        setLikeCount(likes); // Ensure not going below 0
+        setLikeCount((prevCount) => (prevCount ? prevCount - 1 : 0)); // Ensure not going below 0
       } else {
         await likeGame(); // 좋아요 요청
-        setIsLiked(isPrefer)
-        setLikeCount(likes); // Initialize to 1 if null
+        setIsLiked(!isPrefer)
+        setLikeCount((prevCount) => (prevCount ? prevCount + 1 : 1)); // Initialize to 1 if null
 
       }
     } else {
