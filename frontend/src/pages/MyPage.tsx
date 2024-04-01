55404ead47085270462e6105090a12c82bdc2905
JongKook <whdrnrdl789@naver.com>
2024-04-02 02:23:01 +0900

    Feat: Exclude Navbar & MyPage CSS

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,21 +1,10 @@
-// import Navbar from "../components/commonUseComponents/Navbar";
 import Poket from "../components/commonUseComponents/Poket";
 import Profile from "../components/MyPageComponents/Profile";
-// import useUserStore from "../stores/userStore";
 
 function MyPage() {
-  // const {user} = useUserStore();
-  // console.log('MyPage에서 출력한 User ')
-  // console.log(user);
-
   return (
     <>
-      <div>
-        <h1>아무것도 안되는 거야?</h1>
-      </div>
-      {/* <Navbar/> */}
       <div style={{ position: "relative" }}>
-        {/* <Navbar /> */}
         <Poket/>
         <div style={{ position: "absolute", left: "350px", top: '100px' }}>
           <Profile />
