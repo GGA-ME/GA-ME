67110990509628c6ba41e985979d4a993b804e3c
장현욱 <chiru7080@daum.net>
2024-04-02 16:08:35 +0900

    Update frontend/src/components/mainComponents/Banner.tsx

--- frontend/src/components/mainComponents/Banner.tsx
+++ frontend/src/components/mainComponents/Banner.tsx
@@ -3,7 +3,7 @@ import useStoreMain from "../../stores/mainStore";
 import { useNavigate } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Swiper as SwiperClass } from 'swiper/types';
-import { Autoplay, Pagination, Navigation, Thumbs, FreeMode, EffectCoverflow } from 'swiper/modules';
+import { Autoplay, Pagination, Navigation, Thumbs, FreeMode } from 'swiper/modules';
 import style from './Banner.module.css'
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -49,19 +49,9 @@ const Banner: React.FC = () => {
       {/* 베너 메인 */}
       <Swiper
         className={`${style.swiperCustom} w-full h-full`}
-        modules={[FreeMode, Autoplay, Pagination, Navigation, Thumbs, EffectCoverflow]}
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 50,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        slidesPerView={2}
+        modules={[FreeMode, Autoplay, Pagination, Navigation, Thumbs]}
         spaceBetween={0}
+        slidesPerView={1}
         loop={true}
         navigation={true}
         autoplay={{
@@ -83,7 +73,7 @@ const Banner: React.FC = () => {
       {/* 베너 thumbs */}
       <Swiper
         className="mySwiper"
-        modules={[FreeMode, Navigation, Thumbs]}
+        modules={[FreeMode, Navigation]}
         onSwiper={setThumbsSwiper}
         loop={false}
         spaceBetween={10}
