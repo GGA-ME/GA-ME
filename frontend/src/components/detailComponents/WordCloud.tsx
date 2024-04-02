e6f43b720db3f65e39909586a3c326405478930d
ymiiin <qkslzl101216@naver.com>
2024-04-02 20:57:31 +0900

    Add: Img Test
    Img Test

--- frontend/src/components/detailComponents/WordCloud.tsx
+++ frontend/src/components/detailComponents/WordCloud.tsx
@@ -5,13 +5,15 @@ interface WordCloudProps {
   }
 
 const InfoDescription: React.FC<WordCloudProps> = ({ gameWordCloudUrl })  => {
+  gameWordCloudUrl = '/wordCloud/10_wordcloud_20240402.png';
   return (
     <>
       <div className={`${styles.container}`}>
-        <h1 className={styles.reviewTitle}>리뷰 분석</h1>
-      {gameWordCloudUrl ? <img src={gameWordCloudUrl} alt="Game Word Cloud" /> : 
+        <h1 className={styles.reviewTitle}>리뷰 분석</h1> 
+        <p className={styles.reviewDetail}>리뷰에 많이 사용된 단어</p>
+      {gameWordCloudUrl ? <img className={styles.wordCloudImg}src={gameWordCloudUrl} alt="Game Word Cloud" /> : 
       <div className={`${styles.wordCloudContainer}`}>
-            <p> 분석을 할만한 충분한 댓글 데이터가 없습니다 💦</p>
+            <p className={`${styles.noImg}`}> 분석을 할만한 충분한 댓글 데이터가 없습니다 💦</p>
         </div>}
       </div>
     </>
